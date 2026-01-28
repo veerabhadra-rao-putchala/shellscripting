33f54c19e9ab2490e65fb4aa078a3e908c0a8a18
pvraoveera <pvraoveeraster92@gmail.com>
2026-01-28 07:37:38 +0530

    conversation

---
+++ 02-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hello Ramesh, how are u?"
+echo "Ramesh: Hi Suresh, I am fine"
+echo "Suresh: doing fine, what are you doing"
+echo "Ramesh: i am learning shellscripting"
