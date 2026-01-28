ea94ff5191ecabbcdea9ec3f0d71f49ea89a79dd
pvraoveera <pvraoveeraster92@gmail.com>
2026-01-28 07:45:24 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Suresh
+PERSON2=Ramesh
+echo "$PERSON1: Hello Ramesh, how are u?"
+echo "$PERSON2: Hi Suresh, I am fine"
+echo "$PERSON1: doing fine, what are you doing"
+echo "$PERSON2: i am learning shellscripting"
