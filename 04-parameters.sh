5e00546acb160fbce0c21fde3afea63a92c0f544
pvraoveera <pvraoveeraster92@gmail.com>
2026-01-28 07:49:23 +0530

    parameters

---
+++ 04-parameters.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hello Ramesh, how are u?"
+echo "$PERSON2: Hi Suresh, I am fine"
+echo "$PERSON1: doing fine, what are you doing"
+echo "$PERSON2: i am learning shellscripting"
