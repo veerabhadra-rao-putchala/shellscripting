b3d247ab41a8b01cda2a3b5b18db934182eccc5d
pvraoveera <pvraoveeraster92@gmail.com>
2026-01-29 23:31:44 +0530

    actions

---
+++ 12-loops.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+pkginstall()
+{
+    dnf list $1 --installed  &> /dev/null
+    if [ $? -eq 0 ]
+    then   
+        echo "$1 package is already installed" | tee -a $LOGS_FILE
+    else
+        echo
+        echo "$1 installing ..."
+        dnf install $1 -y
+        if [ $? -eq 0 ]
+        then
+            echo "Installing $1 ... SUCCESS" | tee -a $LOGS_FILE
+        else
+            echo "Installing $1 ... FAILURE" | tee -a $LOGS_FILE
+        fi 
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    pkginstall $package -y &>>$LOGS_FILE
+done
