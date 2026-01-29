c8e54b594dbfbe3eacb4cb296b196e5844b517cd
pvraoveera <pvraoveeraster92@gmail.com>
2026-01-29 22:54:17 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
 echo "Installing Nginx"
 dnf install nginx -y
 
