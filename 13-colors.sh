9097b7c498eefbcceaabfe3fe160c7ac429f19f7
pvraoveera <pvraoveeraster92@gmail.com>
2026-01-29 23:56:08 +0530

    actions

---
+++ 13-colors.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+pkginstall()
+{
+    dnf list $1 --installed  &> /dev/null
+    if [ $? -eq 0 ]
+    then   
+        echo "$Y $1 package is already installed $N" | tee -a $LOGS_FILE
+    else
+        echo
+        echo "$R $1 package not installed, $N installing now ..."
+        dnf install $1 -y
+        if [ $? -eq 0 ]
+        then
+            echo "$G Installing $1 ... SUCCESS" | tee -a $LOGS_FILE
+        else
+            echo "$R Installing $1 ... FAILURE" | tee -a $LOGS_FILE
+        fi 
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    pkginstall $package   # &>>$LOGS_FILE
+done
+echo "$Y =============================$G FINISHED ========================= $Y" | tee -a $LOGS_FILE
